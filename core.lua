3acdf8dde8ba8f6e8bd19a09ab461357e92ef5fd
Robbie Bendick <robbiebendick@gmail.com>
2023-03-10 09:18:09 -0800

    hide status text on focus hide

--- core.lua
+++ core.lua
@@ -1,9 +1,9 @@
 local _, core = ...;
 
 -- make our own status text
-local resourceTextFrame = CreateFrame("FRAME")
-resourceTextFrame.text = resourceTextFrame:CreateFontString(nil, "OVERLAY", "TextStatusBarText")
-resourceTextFrame.text:SetPoint("CENTER", FocusFrameManaBar, "CENTER", 0, 0)
+local resourceTextFrame = CreateFrame("FRAME");
+resourceTextFrame.text = resourceTextFrame:CreateFontString(nil, "OVERLAY", "TextStatusBarText");
+resourceTextFrame.text:SetPoint("CENTER", FocusFrameManaBar, "CENTER", 0, 0);
 
 -- hook the mana bar to show our own status text
 FocusFrameManaBar:HookScript("OnValueChanged", function(self)
@@ -11,33 +11,38 @@ FocusFrameManaBar:HookScript("OnValueChanged", function(self)
     local _, powerType = UnitPowerType(self.unit);
     local mana, maxMana = UnitPower(self.unit), UnitPowerMax(self.unit);
     if powerType ~= "MANA" then
-        resourceTextFrame.text:SetText(mana .. "/" .. maxMana)
-        resourceTextFrame.text:Show()
+        resourceTextFrame.text:SetText(mana .. "/" .. maxMana);
+        resourceTextFrame.text:Show();
     else
-        resourceTextFrame.text:Hide()
+        resourceTextFrame.text:Hide();
     end
 end);
 
 -- hide our own status text when status text is displayed
-local frame = CreateFrame("FRAME")
-frame:RegisterEvent("CVAR_UPDATE")
+local frame = CreateFrame("FRAME");
+frame:RegisterEvent("CVAR_UPDATE");
 frame:SetScript("OnEvent", function(self, event, ...)
     if event == "CVAR_UPDATE" then
         local cvar, value = ...;
         if cvar == "STATUS_TEXT_DISPLAY" then
             if value == "NONE" then
-                resourceTextFrame.text:Show()
+                resourceTextFrame.text:Show();
             else
-                resourceTextFrame.text:Hide()
+                resourceTextFrame.text:Hide();
             end
         end
     end
-end)
+end);
 
 -- hide our own status text when entering the world
-resourceTextFrame:RegisterEvent("PLAYER_ENTERING_WORLD")
+resourceTextFrame:RegisterEvent("PLAYER_ENTERING_WORLD");
 resourceTextFrame:SetScript("OnEvent", function(self, event, ...)
     if event == "PLAYER_ENTERING_WORLD" then
         resourceTextFrame.text:Hide();
     end
-end)
+end);
+
+-- hide our own status text when focus frame is hidden
+FocusFrame:HookScript("OnHide", function(self)
+    resourceTextFrame.text:Hide();
+end);
