d6c130416ec46658437343bc8ccd72fa6c541bd4
Robbie Bendick <robbiebendick@gmail.com>
2023-03-10 10:19:12 -0800

    add onshow func

--- core.lua
+++ core.lua
@@ -1,5 +1,43 @@
 local _, core = ...;
 
+local statusTextObj = {
+    ["RAGE"] = function(self, frame)
+        local resource = UnitPower(self.unit);
+        local class = select(2, UnitClass(self.unit));
+        if class ~= "WARRIOR" then return end
+
+        if resource > 10 and resource < 15 then
+            frame.text:SetText("CANNOT REFLECT");
+        elseif resource < 10 then
+            frame.text:SetText("CANNOT KICK");
+        else
+            frame.text:SetText(resource);
+        end
+    end,
+    ["ENERGY"] = function(self, frame)
+        local resource = UnitPower(self.unit);
+        local class = select(2, UnitClass(self.unit));
+        if class ~= "ROGUE" then return end
+
+        if resource < 25 then
+            frame.text:SetText("CANNOT KICK");
+        else
+            frame.text:SetText(resource);
+        end
+    end,
+    ["RUNIC_POWER"] = function(self, frame)
+        local resource = UnitPower(self.unit);
+        local class = select(2, UnitClass(self.unit));
+        if class ~= "DEATHKNIGHT" then return end
+
+        if resource < 20 then
+            frame.text:SetText("CANNOT AMS");
+        else
+            frame.text:SetText(resource);
+        end
+    end
+};
+
 -- make our own status text
 local resourceTextFrame = CreateFrame("FRAME");
 resourceTextFrame.text = resourceTextFrame:CreateFontString(nil, "OVERLAY", "TextStatusBarText");
@@ -29,49 +67,20 @@ resourceTextFrame:SetScript("OnEvent", function(self, event, ...)
     end
 end);
 
+-- show our own status text when focus frame is hidden
+FocusFrame:HookScript("OnShow", function(self)
+    local _, powerType = UnitPowerType(self.unit);
+    local resource = UnitPower(self.unit);
+    if powerType ~= "MANA" then
+        if statusTextObj[powerType] then
+            statusTextObj[powerType](self, resourceTextFrame);
+        end
+        resourceTextFrame.text:Show();
+    end
+end);
+
 -- hide our own status text when focus frame is hidden
 FocusFrame:HookScript("OnHide", function(self) resourceTextFrame.text:Hide() end);
-
--- show our own status text when focus frame is hidden
-FocusFrame:HookScript("OnShow", function(self) resourceTextFrame.text:Show() end);
-
-local statusTextObj = {
-    ["RAGE"] = function(self, frame)
-        local resource = UnitPower(self.unit);
-        if class ~= "WARRIOR" then return end
-
-        if resource > 10 and resource < 15 then
-            frame.text:SetText("CANNOT REFLECT");
-        elseif resource < 10 then
-            frame.text:SetText("CANNOT KICK");
-        else
-            frame.text:SetText(resource);
-        end
-    end,
-    ["ENERGY"] = function(self, frame)
-        local resource = UnitPower(self.unit);
-        local class = select(2, UnitClass(self.unit));
-        if class ~= "ROGUE" then return end
-
-        if resource < 25 then
-            frame.text:SetText("CANNOT KICK");
-        else
-            frame.text:SetText(resource);
-        end
-    end,
-    ["RUNIC_POWER"] = function(self, unit)
-        local resource = UnitPower(self.unit);
-        local class = select(2, UnitClass(self.unit));
-        if class ~= "DEATHKNIGHT" then return end
-
-        -- if the player has less than 25 energy then show text "CANNOT KICK",
-        if resource < 20 then
-            resourceTextFrame.text:SetText("CANNOT AMS");
-        else
-            resourceTextFrame.text:SetText(resource);
-        end
-    end
-}
 
 -- hook the mana bar to show our own status text
 FocusFrameManaBar:HookScript("OnValueChanged", function(self)
